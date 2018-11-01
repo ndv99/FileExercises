0273f63869e2af074e511ed41c9a6b9b5a767d90
Nick <noodles524@gmail.com>
2018-11-01 20:59:52 +0000

    Commenting.

--- src/Files.java
+++ src/Files.java
@@ -1,18 +1,33 @@
 import java.util.Scanner;
 import java.io.*;
 
+/**
+ * Collection of methods for text file I/O.
+ */
 public class Files {
 
+    /**
+     * Main method. Creates instance of Files and calls runFileTests.
+     * @param args args
+     */
     public static void main(String[] args){
         Files files = new Files();
         files.runFileTests();
     }
 
-    public void runFileTests(){
+    /**
+     * Runs I/O methods in this class.
+     */
+    private void runFileTests(){
+        readFromFile();
+        writeToFile();
         copyFile();
     }
 
-    public void readFromFile() {
+    /**
+     * Reads from a file specified by the user and prints the content of it.
+     */
+    private void readFromFile() {
         Scanner askUser = new Scanner(System.in);
 
         boolean fileReadCorrectly = false;
@@ -21,11 +36,14 @@ public class Files {
                 System.out.println("Enter the name of the file you want to read.");
                 String fileName = askUser.nextLine() + ".txt";
 
+                // setting up file reader
                 FileReader fileReader = new FileReader("src/" + fileName);
                 BufferedReader bufferedReader = new BufferedReader(fileReader);
 
+                // grabs first line from file
                 String nextFileLine = bufferedReader.readLine();
 
+                // prints every line in file to console
                 while (nextFileLine != null){
                     System.out.println(nextFileLine);
                     nextFileLine = bufferedReader.readLine();
@@ -39,25 +57,32 @@ public class Files {
          } while (!fileReadCorrectly);
     }
 
-    public void writeToFile() {
+    /**
+     * Writes user input to "out.txt".
+     */
+    private void writeToFile() {
         Scanner userWrite = new Scanner(System.in);
 
+        // setting up file writer
         FileOutputStream outputStream;
         PrintWriter printWriter = null;
 
         try {
+            // specifies output file
             outputStream = new FileOutputStream("out.txt");
             printWriter = new PrintWriter(outputStream);
 
             String line;
+
             do {
                 System.out.println("Enter something to be written to the file, or hit Enter to finish.");
                 line = userWrite.nextLine();
                 printWriter.println(line);
-            } while (!line.isEmpty());
-        } catch (IOException e) {
+            } while (!line.isEmpty()); // keeps going until user enters empty line
+        } catch (IOException e) { // in case something goes wrong
             System.out.println("Something has gone wrong!");
         } finally {
+            // printWriter will be null if something has gone wrong
             if (printWriter != null) {
                 System.out.println("Written to out.txt.");
                 printWriter.close();
@@ -65,27 +90,38 @@ public class Files {
         }
     }
 
-    public void copyFile() {
+    /**
+     * Copies the content of one file to another, the names of which are user-specified.
+     */
+    private void copyFile() {
+
         Scanner askUser = new Scanner(System.in);
         boolean noErrors = false;
+
+        // setup for file writer
         FileOutputStream outputStream;
-        PrintWriter printWriter = null;
+        PrintWriter printWriter;
+
         do {
             try {
                 System.out.println("Enter the name of the file you want to copy.");
                 String sourceFileName = askUser.nextLine() + ".txt";
 
+                // sets up reader from specified input file
                 FileReader fileReader = new FileReader("src/" + sourceFileName);
                 BufferedReader bufferedReader = new BufferedReader(fileReader);
 
                 System.out.println("Enter the name of the file you want to copy to.");
                 String destinationFileName = askUser.nextLine() + ".txt";
 
+                // output file is set to what user specifies
                 outputStream = new FileOutputStream(destinationFileName);
                 printWriter = new PrintWriter(outputStream);
 
+                // grabs first line from input file
                 String nextFileLine = bufferedReader.readLine();
 
+                // goes through entire file until end is reached
                 while (nextFileLine != null){
                     printWriter.println(nextFileLine);
                     nextFileLine = bufferedReader.readLine();
