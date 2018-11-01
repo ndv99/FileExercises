2fb1c732fe0747965db7717e6c5ffa5cfdcac807
Nick <noodles524@gmail.com>
2018-11-01 20:02:09 +0000

    Finished method to write file.

--- src/Files.java
+++ src/Files.java
@@ -9,7 +9,7 @@ public class Files {
     }
 
     public void runFileTests(){
-        askForFile();
+        writeToFile();
     }
 
     public void askForFile() {
@@ -34,13 +34,35 @@ public class Files {
                 bufferedReader.close();
                 fileReadCorrectly = true;
             } catch (IOException e) {
-                System.out.println("Something has gone wrong!");
+                System.out.println("That file doesn't exist.");
             }
          } while (!fileReadCorrectly);
     }
 
-    public void writetoFile() {
+    public void writeToFile() {
         Scanner userWrite = new Scanner(System.in);
+
+        FileOutputStream outputStream;
+        PrintWriter printWriter = null;
+
+        try {
+            outputStream = new FileOutputStream("out.txt");
+            printWriter = new PrintWriter(outputStream);
+
+            String line;
+            do {
+                System.out.println("Enter something to be written to the file, or hit Enter to finish.");
+                line = userWrite.nextLine();
+                printWriter.println(line);
+            } while (!line.isEmpty());
+        } catch (IOException e) {
+            System.out.println("Something has gone wrong!");
+        } finally {
+            if (printWriter != null) {
+                System.out.println("Written to out.txt.");
+                printWriter.close();
+            }
+        }
     }
 
     public void copyFile() {
