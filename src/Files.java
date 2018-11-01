0b783c5b6f89dbb19f10fc7bb3b418ac8b3021d1
Nick <noodles524@gmail.com>
2018-11-01 16:37:46 +0000

    Created method to ask user to enter filename to read from.

--- src/Files.java
+++ src/Files.java
@@ -1,2 +1,42 @@
+import java.util.Scanner;
+import java.io.*;
+
 public class Files {
+
+    public static void main(String[] args){
+        Files files = new Files();
+        files.runFileTests();
+    }
+
+    public void runFileTests(){
+        askForFile();
+    }
+
+    public void askForFile() {
+        String fileName = ".txt";
+        Scanner askUser = new Scanner(System.in);
+
+        System.out.println("Enter the name of the file you want to read.");
+        fileName = askUser.nextLine() + fileName;
+        boolean fileReadCorrectly = false;
+        do{
+            try{
+                FileReader fileReader = new FileReader("src/" + fileName);
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+                String nextFileLine = bufferedReader.readLine();
+
+                while (nextFileLine != null){
+                    System.out.println(nextFileLine);
+                    nextFileLine = bufferedReader.readLine();
+                }
+
+                bufferedReader.close();
+                fileReadCorrectly = true;
+            } catch (IOException e) {
+                System.out.println("Something has gone wrong!");
+            }
+         } while (!fileReadCorrectly);
+    }
+
 }
